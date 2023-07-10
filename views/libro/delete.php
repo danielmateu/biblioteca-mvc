7727bfeb3ed36da46799684a67a4c116d4a604ff
Daniel Mateu <danielmateu86@gmail.com>
2023-07-10 12:12:56 +0200

    Eliminando portada al eliminar libro

--- views/libro/delete.php
+++ views/libro/delete.php
@@ -7,6 +7,7 @@
     <title><?= APP_NAME ?> - Eliminación Libro</title>
     <link rel="stylesheet" href="/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="/js/Preview.js"></script>
 </head>
 
 <body>
@@ -23,6 +24,13 @@
         <h2>Quieres eliminar <?= $libro->titulo ?>?</h2>
 
         <!-- Formulario para edicion de libro -->
+
+
+        <div class="card my-4">
+            <img src="<?= BOOK_IMAGE_FOLDER . '/' . ($libro->portada ?? DEFAULT_BOOK_IMAGE) ?> " alt="Portada del libro" class="card-img-top p-2" width="100px">
+
+        </div>
+
         <form class="form" method="POST" action="/Libro/destroy">
             <!-- <h2>Creación de Libros</h2> -->
 
@@ -32,16 +40,18 @@
             <input type="submit" value="Eliminar Libro" class="btn btn-danger" name="borrar" value="Borrar">
         </form>
 
-        <div class="d-flex justify-content-center gap-2">
-            <!-- Botones para volver, editar y borrar -->
-            <a class="btn btn-primary" href="/libro">Volver</a>
-            <a class="btn btn-secondary" href="/libro/edit/<?= $libro->id ?>">Editar</a>
-            <!-- <a class="btn btn-danger" href="/libro/delete/<?= $libro->id ?>">Borrar</a> -->
-        </div>
+
 
 
 
     </main>
+
+    <div class="d-flex justify-content-center gap-2">
+        <!-- Botones para volver, editar y borrar -->
+        <a class="btn btn-primary" href="/libro">Volver</a>
+        <a class="btn btn-secondary" href="/libro/edit/<?= $libro->id ?>">Editar</a>
+        <!-- <a class="btn btn-danger" href="/libro/delete/<?= $libro->id ?>">Borrar</a> -->
+    </div>
 
     <?= Template::getAltFooter() ?>
 </body>
